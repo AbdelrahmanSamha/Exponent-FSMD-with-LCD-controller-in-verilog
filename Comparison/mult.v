036053360da2ae3f9c979d9c80d4f2fdae7545e3
AbdelrahmanSamha <AbdelrahmanSamha@outlook.com>
2025-05-22 19:10:03 +0300

    Delete Comparison directory

--- Comparison/mult.v
+++
@@ -1,9 +0,0 @@
-module mult (
-	input [15:0] a, b, 
-	output [15:0] product 
-
-);
-
-assign product = a * b;
-
-endmodule 
--- Comparison/subtractor.v
+++
@@ -1,6 +0,0 @@
-module subtractor (input[7:0] in, output[7:0] out );
-
-assign out = in - 8'b1; 
-
-
-endmodule 
